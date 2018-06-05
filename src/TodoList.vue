dc44892aca6f70b3a435987325a39a0951c8bcff
libowen <libw@nroad.com.cn>
2018-06-06 00:05:11 +0800

    add all

--- src/TodoList.vue
+++ src/TodoList.vue
@@ -1,14 +1,18 @@
 <template>
-<div>
-  <div>
-    <input v-model="inputValue">
-    <button @click="handleSubmit">提交</button>
-  </div>
-  <ul>
-    <todo-item v-for = "(item,index) of list" :key="index"  :content="item" :index="index" @delete-item="handleDelete">
-    </todo-item>
+  <div class="container" >
+    <div class="add-task">
+      <!--<input v-model="inputValue">-->
+      <h1>to do my list!</h1>
+      <input v-model="inputValue" name="content" type="text"  placeholder="e.g.今天记得学习"
+             autofocus="autofocus" autocomplete="off">
+      <button @click="handleSubmit" type="submit">提交</button>
+      <button @click="handleDeleteAll" type="submit">删除全部</button>
+    </div>
 
-  </ul>
+    <todo-item v-for = "(item,index) of list" :key="index"  :content="item" :index="index"
+               @delete-item="handleDelete"
+      @change-item="handleChange">
+    </todo-item>
 </div>
 </template>
 
@@ -28,17 +32,68 @@ export default {
     },
     methods :{
         handleSubmit (){
-           this.list.push(this.inputValue)
-            //this的指向是当前的实例
-            this.inputValue = ''
+              if(!this.inputValue == ''){
+                this.list.unshift(this.inputValue)
+                //this的指向是当前的实例
+                this.inputValue = ''
+              }
         },
         handleDelete (index){
             this.list.splice(index,1)
-        }
+        },
+        handleDeleteAll(){
+            this.list.length = 0;
+        },
+      handleChange(index){
+            console.log(index)
+         this.list.hidden
+//            console.log(.cssText)
+//              .cssText("color","green")
+      }
     }
 }
 </script>
 
 <style>
-
+ .change{
+   background-color: aqua;
+   color:darkolivegreen;
+ }
+  *{
+    /*background: rgba(0,0,0,.1);*/
+    box-sizing: border-box;
+    /*transition是什么作用*/
+    transition: background-color .2s;
+    outline: 0;
+  }
+ body{
+   background-color: lightpink;
+ }
+ .container{
+   max-width: 700px;
+   margin: 0 auto;/*设置居中*/
+   position: relative;/*为父元素添加relative，子元素设置absolute*/
+   padding: 0 10px ;
+ }
+ h1{
+   text-align: center;
+ }
+ .add-task input[type=text]{
+   float: left;
+   width: 65%;
+   margin-right: 1%;
+ }
+ .add-task [type=submit]{
+   width: 15%;
+   -webkit-box-shadow: 0 2px 3px rgba(0,0,0,.4);
+ }
+ .add-task [type=submit]:hover{
+   background: #b7a0ae;
+ }
+ input,button{
+   border-radius: 3px;
+   padding: 10px;
+   border: 0;
+ }
 </style>
+
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,24 +1,65 @@
 <template>
-    <li class="item" v-on:click ="handleDelete">{{content}}</li>
+  <div class="task-list">
+    <div class="task-item">
+      <span><input class="complete" type="checkbox" @change = "handleCheck"></span>
+      <span class="task-content">{{content}} {{currentTime}}</span>
+      <span class="fr">
+        <span class="action delete" @click ="handleDelete"> 删除</span>
+        <span class="action detail"> 详细</span>
+        </span>
+    </div>
+  </div>
 </template>
+
 
 <script>
 export default {
     props:['content','index'],
+    data(){
+      var date = new Date()
+      var month = date.getMonth()+1
+      var day = date.getDate();
+      var time = date.getHours();
+      var minutes = date.getMinutes();
+      var seconds = date.getSeconds();
+      return {
+          currentTime: month+":"+day+"-"+time+":"+minutes+":"+seconds
+      }
+    },
     methods: {
         handleDelete(){
             //$emit触发当前实例上的事件。附加参数都会传给监听器回调。
             this.$emit('delete-item',this.index)
+        },
+        handleCheck(){
+            this.$emit('change-item',this.index)
         }
     }
 }
 </script>
 
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     /*<!--scoped为样式添加了一个域  这里的样式只适应于本组件-->*/
-    .item{
-        color: skyblue;
+    .task-list{
+      margin: 10px 0;
     }
-
+    .task-item{
+      padding: 10px;
+      background-color: #fff;
+      margin-bottom: 2px;
+      /*设置阴影*/
+      -webkit-box-shadow: 0 2px 3px rgba(0,0,0,.4);
+      /*设置一个小手出来  表示这里可以点击*/
+      cursor:pointer;
+      border-radius: 3px;
+      border: 0;
+    }
+    .task-item:hover{
+      background: #eee;
+    }
+    .fr{
+      float: right;
+    }
 </style>
